ff3ab26c8333dcbfd7a7e592bd306cb5ecc64d3c
jmmathewsiii <joma6354@colorado.edu>
2026-03-02 19:30:26 -0700

    hodlr start

---
+++ hodlr/include/generators.h
@@ -0,0 +1,9 @@
+#ifndef GENERATORS_H
+#define GENERATORS_H
+
+#include <vector>
+
+std::vector<double> generateLeafMatrix(int);
+
+
+#endif // !GENERATORS_H
---
+++ hodlr/src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include "../include/generators.h"
+
+int main(int argc, char** argv)
+{
+   int n = std::stoi(argv[1]); // Size of leaf nodes: n x n
+   int k = std::stoi(argv[2]); // Rank of low-rank off-diagonal blocks
+   int L = std::stoi(argv[3]); // Number of levels
+
+  // Generate 2^L leaf matrices, each of size n x n 
+
+    return 0;
+}
